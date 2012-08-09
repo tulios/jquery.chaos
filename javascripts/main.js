e5ef6b775e61f1abd130c35d86de1bc16b9c09df
tulios <ornelas.tulio@gmail.com>
2012-08-09 00:56:46 -0300

    improved demo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,18 @@
-console.log('This would be the main JS file.');
+$(function() {
+  $("#container").chaos();
+
+  $("#mess").click(function(e) {
+    e.preventDefault();
+    $("#container").chaos().original();
+  });
+
+  $("#organize").click(function(e) {
+    e.preventDefault();
+    $("#container").chaos().organize();
+  });
+
+  $("#organize-reverse").click(function(e) {
+    e.preventDefault();
+    $("#container").chaos().organize({order: "reverse"});
+  });
+});
